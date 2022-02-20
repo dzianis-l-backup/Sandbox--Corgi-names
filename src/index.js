fab6de6ab0d98c42da4803e6262e993c9ea381e3
Dzianis Lean <leadzian213@gmail.com>
2022-02-20 17:23:52 +0300

    build: test coverage with c8

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import uniqueRandomArray from 'unique-random-array'
-// import { readFileSync } from 'fs'
-// import path from 'path'
-// const starWarsNames = JSON.parse(readFileSync(new URL('./names.json', import.meta.url)))
-
 import starWarsNames from './names.json'
-
-// import { fileURLToPath } from 'url'
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// const starWarsNames = JSON.parse(readFileSync(path.resolve(__dirname, './names.json'), { encoding: 'utf-8' }))
 
 export default {
     all: starWarsNames,
